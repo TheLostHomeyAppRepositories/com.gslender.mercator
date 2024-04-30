69509a843e3644b99cc3e6f0a446d725598fdd2b
Grant Slender <gslender@gmail.com>
2024-04-30 10:29:31 +1000

    Final update and all fan / light modes working

--- drivers/fan_light_sswf01g/device.js
+++ drivers/fan_light_sswf01g/device.js
@@ -4,13 +4,31 @@ const { ZigBeeDevice } = require("homey-zigbeedriver");
 const { ZCLNode, CLUSTER, debug } = require('zigbee-clusters');
 
 // Enable debug logging of all relevant Zigbee communication
-debug(true);
+// debug(true);
 
 const IkuuFanSpeedCluster = require('../../lib/IkuuFanSpeedCluster');
+
+const SPEEDS = {
+  off: 0,
+  low: 1,
+  medium: 2,
+  high: 3,
+  on: 4
+};
+
+function getSpeedValue(speedKey) {
+  const key = speedKey.trim().toLowerCase(); // Clean and standardize the input
+  if (key in SPEEDS) {
+    return SPEEDS[key];
+  } else {
+    throw new Error("Invalid speed key"); // Handle unknown keys
+  }
+}
 
 class FanLightDevice extends ZigBeeDevice {
 
   async onNodeInit({ zclNode }) {
+
     this.log('FanLightDevice has been initialized');
 
     this.registerCapability("onoff", CLUSTER.ON_OFF, {
@@ -23,13 +41,25 @@ class FanLightDevice extends ZigBeeDevice {
       },
     });
 
-    this.registerCapability("fanspeed", IkuuFanSpeedCluster, {
-      setSpeed: async (value) => {
-        this.log('FanLightDevice setSpeed',value);
-      }
+    this.registerCapabilityListener("fanspeed",async (value, opts) => {
+      await zclNode.endpoints[1].clusters[IkuuFanSpeedCluster.NAME].writeAttributes({speed:getSpeedValue(value)});
     });
 
-    // this.registerCapabilityListener("fanspeed",)
+     await zclNode.endpoints[1].clusters[IkuuFanSpeedCluster.NAME].configureReporting({
+      speed: {
+        minInterval: 0,
+        maxInterval: 300,
+        minChange: 1,
+      },
+    });
+
+    zclNode.endpoints[1].clusters[IkuuFanSpeedCluster.NAME].on(
+      "attr.speed",
+      (value) => {
+        this.setCapabilityValue("fanspeed",value);
+      }
+    );
+
     
   }
 }

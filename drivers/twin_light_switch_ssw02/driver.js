ad11527388df2bf2006f5625110568cf7d73f4c9
Grant Slender <gslender@gmail.com>
2024-06-03 18:00:41 +1000

    twin light device

---
+++ drivers/twin_light_switch_ssw02/driver.js
@@ -0,0 +1,10 @@
+
+'use strict';
+
+const { ZigBeeDriver } = require('homey-zigbeedriver');
+
+class triple_light_switch_ssw03 extends ZigBeeDriver {
+
+}
+
+module.exports = triple_light_switch_ssw03;

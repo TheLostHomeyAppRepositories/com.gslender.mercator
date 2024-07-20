febcfa927854477f68c048d40359c2f06e00a51b
Grant Slender <gslender@gmail.com>
2024-07-20 21:45:19 +1000

    v1.1.9 Five light Tuya DP fix

--- drivers/five_light_switch_ssw05/device.js
+++ drivers/five_light_switch_ssw05/device.js
@@ -1,7 +1,6 @@
 'use strict';
 
-const { ZigBeeDevice } = require("homey-zigbeedriver");
-const { CLUSTER,Cluster, debug } = require('zigbee-clusters');
+const { CLUSTER, Cluster, debug } = require('zigbee-clusters');
 const TuyaSpecificCluster = require('../../lib/TuyaSpecificCluster');
 const TuyaSpecificClusterDevice = require("../../lib/TuyaSpecificClusterDevice");
 
@@ -10,35 +9,98 @@ Cluster.addCluster(TuyaSpecificCluster);
 class FiveLightSwitchDevice extends TuyaSpecificClusterDevice {
 
   async onNodeInit({ zclNode }) {
-    await super.onNodeInit({ zclNode });
+    super.onNodeInit({ zclNode });
 
-    this.log('FiveLightSwitchDevice has been initialized');
+    this.printNode();
+    debug(true);
 
-    const { subDeviceId } = this.getData();
-    this.log('Device data: ', subDeviceId);
+    const subDeviceId = this.isSubDevice() ? this.getData().subDeviceId : 'firstLight';
+    this.log('FiveLightSwitchDevice ', subDeviceId, ' has been initialized');
 
-    this.registerCapability('onoff', CLUSTER.ON_OFF, {
-      endpoint: subDeviceId === 'secondLight' ? 2 : subDeviceId === 'thirdLight' ? 3 : subDeviceId === 'fourthLight' ? 4 : subDeviceId === 'fifthLight' ? 5: 1,
-    });
-
-    if (!this.isSubDevice()) {
-        await zclNode.endpoints[1].clusters.basic.readAttributes(['manufacturerName', 'zclVersion', 'appVersion', 'modelId', 'powerSource', 'attributeReportingStatus'])
+    if (subDeviceId === 'firstLight') {
+      await zclNode.endpoints[1].clusters.basic.readAttributes(['manufacturerName', 'zclVersion', 'appVersion', 'modelId', 'powerSource', 'attributeReportingStatus'])
         .catch(err => {
-            this.error('Error when reading device attributes ', err);
+          this.error('Error when reading device attributes ', err);
         });
+      this.registerCapabilityListener('onoff', async value => {
+        // this.log('onoff: ', value);
+        await this.writeBool(1, value);
+      });
     }
-    // debug(true);
 
-    // this.printNode();
-    // console.log(zclNode.endpoints);
+    if (subDeviceId === 'secondLight') {
+      this.registerCapabilityListener('onoff', async value => {
+        // this.log('onoff: ', value);
+        await this.writeBool(2, value);
+      });
+    }
 
+    if (subDeviceId === 'thirdLight') {
+      this.registerCapabilityListener('onoff', async value => {
+        // this.log('onoff: ', value);
+        await this.writeBool(3, value);
+      });
+    }
+
+    if (subDeviceId === 'fourthLight') {
+      this.registerCapabilityListener('onoff', async value => {
+        // this.log('onoff: ', value);
+        await this.writeBool(4, value);
+      });
+    }
+
+    if (subDeviceId === 'fifthLight') {
+      this.registerCapabilityListener('onoff', async value => {
+        // this.log('onoff: ', value);
+        await this.writeBool(5, value);
+      });
+    }
+
+
+    zclNode.endpoints[1].clusters[TuyaSpecificCluster.NAME].on(
+      "response",
+      async (value) => {
+
+        if (value.dp === 1 && subDeviceId === 'firstLight') {
+          const onoff = value.data.readUInt8(0);
+          // this.log(">>>tuya.onoff", onoff === 1 );
+          return await this.setCapabilityValue("onoff", onoff === 1);
+        }
+
+        if (value.dp === 2 && subDeviceId === 'secondLight') {
+          const onoff = value.data.readUInt8(0);
+          // this.log(">>>tuya.onoff", onoff === 1 );
+          return await this.setCapabilityValue("onoff", onoff === 1);
+        }
+
+        if (value.dp === 3 && subDeviceId === 'thirdLight') {
+          const onoff = value.data.readUInt8(0);
+          // this.log(">>>tuya.onoff", onoff === 1 );
+          return await this.setCapabilityValue("onoff", onoff === 1);
+        }
+
+        if (value.dp === 4 && subDeviceId === 'fourthLight') {
+          const onoff = value.data.readUInt8(0);
+          // this.log(">>>tuya.onoff", onoff === 1 );
+          return await this.setCapabilityValue("onoff", onoff === 1);
+        }
+
+        if (value.dp === 5 && subDeviceId === 'fifthLight') {
+          const onoff = value.data.readUInt8(0);
+          // this.log(">>>tuya.onoff", onoff === 1 );
+          return await this.setCapabilityValue("onoff", onoff === 1);
+        }
+
+        this.log(">>>tuya.response", value);
+      }
+    );
+
+    await this.sendQuery();
   }
-
 
   onDeleted() {
-    this.log("FiveLightSwitchDevice removed")
+    this.log('FiveLightSwitchDevice removed');
   }
-
 }
 
 module.exports = FiveLightSwitchDevice;

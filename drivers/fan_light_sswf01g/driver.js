e4ed88c0ff7c5d6ae5fa05beaecbba6abd56c526
Grant Slender <gslender@gmail.com>
2024-10-13 15:26:20 +1000

    v1.1.12 Fix fan flow

--- drivers/fan_light_sswf01g/driver.js
+++ drivers/fan_light_sswf01g/driver.js
@@ -1,5 +1,15 @@
 const Homey = require('homey');
 const IkuuFanSpeedCluster = require('../../lib/IkuuFanSpeedCluster');
+// const stringify = require('json-stringify-safe');
+//"json-stringify-safe": "^5.0.1",
+
+const SPEEDS = {
+  off: 0,
+  low: 1,
+  medium: 2,
+  high: 3,
+  on: 4
+};
 
 function getSpeedValue(speedKey) {
   const key = speedKey.trim().toLowerCase(); // Clean and standardize the input
@@ -18,7 +28,7 @@ class Driver extends Homey.Driver {
 
     const switchFanToAction = this.homey.flow.getActionCard('switch-fan-to');
     switchFanToAction.registerRunListener(async (args, state) => {
-      await args.device.zclNode.endpoints[1].clusters[IkuuFanSpeedCluster.NAME].writeAttributes({ speed: getSpeedValue(args) });
+      await args.device.zclNode.endpoints[1].clusters[IkuuFanSpeedCluster.NAME].writeAttributes({ speed: getSpeedValue(args.Speed) });
     });
   }
 
